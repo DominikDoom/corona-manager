bfc5312f2f2959d936ad418fb022e3c8f06a1bd0
DominikDoom <34448969+DominikDoom@users.noreply.github.com>
2018-01-31 09:22:29 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
-  $("#addbutton").click(function(){
+	$("#addcat").click(function(){
+		var data = {
+			id: uuidv4()
+		}
+		var template = $("#cat-template").html();
+		console.log(template);		
+		var html = Mustache.render(template, data);
+		$("#categorycontainer").append(html);
+		console.log("added");
+	});
+
+	$(document).on('click', "#removecat", function() {
+		if (confirm('Wirklich löschen? Diese Aktion kann nicht rückgängig gemacht werden!')) {
+			$(this).parent().parent().remove();
+			console.log("removed");
+		}
+	});
+
+
+  	$(document).on('click', "#addcard", function() {
 		var data = {
 			id: uuidv4()
 		}
 		var template = $("#card-template").html();
 		console.log(template);		
 		var html = Mustache.render(template, data);
-		$("#categorycontainer").append(html);
+		$(this).parent().parent().find("div[id='cardcontainer']").append(html);
 		console.log("added");
+	});
+
+	$(document).on('click', "#removecard", function() {
+		if (confirm('Wirklich löschen? Diese Aktion kann nicht rückgängig gemacht werden!')) {
+			$(this).parent().remove();
+			console.log("removed");
+		}
 	});
 });
 

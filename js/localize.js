797903c3ac11e78a52214ae79229d74d37630e66
DominikDoom <34448969+DominikDoom@users.noreply.github.com>
2018-03-21 09:22:28 +0100

    Localization prototype
    - This breaks when used after loadConstructor() !

---
+++ js/localize.js
@@ -0,0 +1,100 @@
+// Initialize language data
+var en = {
+    catName: "Name",
+    editCatInfo: "(Click to edit)",
+    addCard: "Add a card",
+    deleteCat: "Delete category",
+    openCard: "Open card",
+    editCard: "Edit card",
+    upCard: "Move up",
+    downCard: "Move down",
+    dialogBoxTitle: "Confirm deletion",
+    dialogNo: "No",
+    dialogYes: "Yes",
+    editorSave: "Save",
+    editorCancel: "Cancel",
+    debugIdDiplay: "Id of selected card: ",
+    editorInputName: "Name",
+    editorInputDesc: "Description",
+    editorImageSelectText: "Select an image",
+    editorImageSelectTTip: "Open image",
+    editorImageReset: "Reset image",
+    editorAdvanced: "Advanced",
+    removeCard: "Remove this card",
+    addCat: "Add a category",
+    viewIconsLabel: "View",
+    cardViewButton: "Card view",
+    listViewButton: "List view",
+    sortIconsLabel: "Sorting",
+    alphaSortButton: "Alphabetic",
+    dateSortButton: "Creation date",
+    autoSortLabel: "Auto-sorting",
+    autoSortButton: "Enable / disable automatic sorting"
+}
+var de = {
+    catName: "Name",
+    editCatInfo: "(Klicken zum Ändern)",
+    addCard: "Karte hinzufügen",
+    deleteCat: "Kategorie löschen",
+    openCard: "Karte öffnen",
+    editCard: "Karte bearbeiten",
+    upCard: "Nach oben",
+    downCard: "Nach unten",
+    dialogBoxTitle: "Wirklich löschen?",
+    dialogNo: "Nein",
+    dialogYes: "Ja",
+    editorSave: "Speichern",
+    editorCancel: "Abbrechen",
+    debugIdDiplay: "Id der ausgewählten Karte: ",
+    editorInputName: "Name",
+    editorInputDesc: "Beschreibung",
+    editorImageSelectText: "Bild auswählen",
+    editorImageSelectTTip: "Bild öffnen",
+    editorImageReset: "Bild zurücksetzen",
+    editorAdvanced: "Erweitert",
+    removeCard: "Karte entfernen",
+    addCat: "Kategorie hinzufügen",
+    viewIconsLabel: "Ansicht",
+    cardViewButton: "Karten",
+    listViewButton: "Liste",
+    sortIconsLabel: "Sortierung",
+    alphaSortButton: "Alphabetisch",
+    dateSortButton: "Erstelldatum",
+    autoSortLabel: "Auto-Sortierung",
+    autoSortButton: "Auto-Sortierung an- / ausschalten"
+}
+
+
+
+function switchLang(lang) {
+    var localizable = $(document).find(".loc");
+    switch (lang) {
+        case "en":
+            $.each(localizable, function(index, element) {
+                if (typeof $(element).attr("title") !== typeof undefined) {
+                    var langString = $(element).attr("loc");
+                    langString = langString.substr(1);
+                    $(element).attr("title",en[langString]);
+                } else {
+                    var langString = $(element).attr("loc");
+                    langString = langString.substr(1);
+                    $(element).text(en[langString]);
+                }
+            });
+            break;
+    
+        case "de":
+            $.each(localizable, function(index, element) {
+                if (typeof $(element).attr("title") !== typeof undefined) {
+                    var langString = $(element).attr("loc");
+                    langString = langString.substr(1);
+                    $(element).attr("title",de[langString]);
+                } else {
+                    var langString = $(element).attr("loc");
+                    langString = langString.substr(1);
+                    $(element).text(de[langString]);
+                }
+            });
+            break;
+    }
+}

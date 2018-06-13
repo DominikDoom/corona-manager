d626729009259b274609fc598e097728ca8b6441
DominikDoom <34448969+DominikDoom@users.noreply.github.com>
2018-06-13 10:48:26 +0200

    jsPlumb testing
    - Only some basic code for console usage + documentation links, basically a To Do list for a later date

---
+++ js/connector.js
@@ -0,0 +1,19 @@
+/* 
+https://jsplumbtoolkit.com/community/doc/dragging.html
+https://jsplumbtoolkit.com/community/apidocs/classes/jsPlumb.html#method_repaint 
+*/
+
+/* 
+jsPlumb.connect({
+    source:$(".pinboardObject")[0], 
+    target:$(".pinboardObject")[1],
+    anchors:["Right", "Left" ],
+    endpoint:"Rectangle",
+    endpointStyle:{ fill: "yellow" }
+}); 
+*/
+
+/* 
+On Drag:
+jsPlumb.repaintEverything()
+*/
